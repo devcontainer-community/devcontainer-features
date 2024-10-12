1365c490412f3d0c8d1576af169306793591cd02
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-10-12 21:19:52 +0200

    feat(`zellij.dev`): Add feature.

---
+++ test/zellij.dev/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+# Provides the 'check' and 'reportResults' commands.
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
+check "execute command" bash -c "zellij --version | grep 'zellij'"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults 
