68eb47663b06a28e6ac3b7e7d66cccfffe09b7f7
Bas Steins <592313+sebst@users.noreply.github.com>
2025-09-12 22:40:48 +0200

    fix(`opencode.ai`): fix test.

--- test/opencode.ai/test.sh
+++ test/opencode.ai/test.sh
@@ -12,7 +12,8 @@ source dev-container-features-test-lib
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
 
-check "execute command" bash -c "opencode --version | grep 'opencode'"
+# check "execute command" bash -c "opencode"
+check "check existence" bash -c "ls -lah /usr/local/bin/opencode"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
