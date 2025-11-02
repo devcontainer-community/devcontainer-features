c82a85b72d73fcead21b7eed4e3de6edb317a979
Bas Steins <592313+sebst@users.noreply.github.com>
2025-11-02 15:38:58 +0000

    update to 0.0.4

--- src/gum-nano-test/install.sh
+++ src/gum-nano-test/install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
+
+
 . ./ensure_nanolayer
-ensure_nanolayer nanolayer_location "v0.0.3"
+ensure_nanolayer nanolayer_location "v0.0.4"
+
+echo $nanolayer_location
+
+$nanolayer_location version
 
 
 $nanolayer_location install github \
-    charmbracelet/gum \
+    "charmbracelet/gum" \
     --asset-url-template 'https://github.com/${Repo}/releases/download/v${Version}/${AssetName}_${Version}_Linux_${Architecture}.tar.gz' \
     --architecture-replacement "arm64 arm64" \
-    --file-destination "*/gum /usr/local/bin/gum"
+    --file-destination "*/gum /tmp/bin/gum" \
+    --asset-name 'gum' \
+    --asset-version "latest"
 
     # --asset-name 'gum' \
+    # --version
 
 # $nanolayer_location system
